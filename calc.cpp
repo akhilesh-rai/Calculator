cd05d96a2e0bb34fc8b39856c636d742f4707dc5
Akhilesh Rai <akhilesh.rai@gmx.de>
2018-08-21 19:11:39 +0200

    added comments

--- calc.cpp
+++ calc.cpp
@@ -8,6 +8,8 @@ int main(void) {
      cout << "Hello World! Printed from myFirstFeatureBranch" << endl;
 
      cout << "This line was added on Master" << endl;
+	
+	/*This comment was added in myFirstBranch, this branch is based off master, and has one child branch*/
 
      return(0);
 
